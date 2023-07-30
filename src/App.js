97cee7accb6b29b41df81b4095d53c985a49b07c
Evgeny Strelnikov <estrelnikov1@yandex.ru>
2023-07-30 21:00:37 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test GIT</h1>
+      <h3> Title</h3>
+      <h5> Subtitle</h5>
     </div>
   );
 }
